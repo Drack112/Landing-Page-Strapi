ee1ab2415c32031b2ac4a0604de5a7630e9437ab
João Vitor <61944386+Drack112@users.noreply.github.com>
2023-01-30 17:46:31 -0300

    ✈ feat: sectionTech with query on CMS Server

--- frontend/src/components/SectionTech/index.tsx
+++ frontend/src/components/SectionTech/index.tsx
@@ -7,12 +7,12 @@ import * as S from './styles'
 import { SectionTechProps } from 'types/api'
 import { getImageUrl } from 'utils/getImageUrl'
 
-const SectionTech = ({ techIcons, title }: SectionTechProps) => (
+const SectionTech = ({ title, techIcons }: SectionTechProps) => (
   <S.Wrapper>
     <Container>
       <Heading reverseColor>{title}</Heading>
       <S.IconsContainer>
-        {techIcons.map(({ icon, title }) => (
+        {techIcons.map(({ title, icon }) => (
           <S.Icon key={title}>
             <S.Icons src={getImageUrl(icon.url)} alt={title} loading="lazy" />
             <S.IconsName>{title}</S.IconsName>
