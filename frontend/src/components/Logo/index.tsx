95c251987d2e9e9d033bcad8d86d181e0d39e043
João Vitor <61944386+Drack112@users.noreply.github.com>
2023-01-30 12:13:09 -0300

    🛠 refactor: using env variables on project

--- frontend/src/components/Logo/index.tsx
+++ frontend/src/components/Logo/index.tsx
@@ -3,7 +3,10 @@ import { LogoProps } from 'types/api'
 import * as S from './styles'
 
 const Logo = ({ url, alternativeText }: LogoProps) => (
-  <S.LogoWrapper src={`http://localhost:1337` + url} alt={alternativeText} />
+  <S.LogoWrapper
+    src={process.env.NEXT_PUBLIC_IMAGE_HOST + url}
+    alt={alternativeText}
+  />
 )
 
 export default Logo
