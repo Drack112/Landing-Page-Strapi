b80c3838a81958926528a519633086a10f2df783
João Vitor <61944386+Drack112@users.noreply.github.com>
2023-01-30 12:16:47 -0300

    🌍 refactor: convert the images url on method

--- frontend/src/components/Logo/index.tsx
+++ frontend/src/components/Logo/index.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import { LogoProps } from 'types/api'
+import { getImageUrl } from 'utils/getImageUrl'
 import * as S from './styles'
 
 const Logo = ({ url, alternativeText }: LogoProps) => (
-  <S.LogoWrapper
-    src={process.env.NEXT_PUBLIC_IMAGE_HOST + url}
-    alt={alternativeText}
-  />
+  <S.LogoWrapper src={getImageUrl(url)} alt={alternativeText} />
 )
 
 export default Logo
